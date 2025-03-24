3cb23abb8165bcca7749fdfb33dddbfa535e1630
oncorphinchus <tri.nguyen@bbzbl-it.ch>
2025-03-24 15:57:58 +0100

    Fix prepared statement error with connection pooling

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,14 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  log: ['error'],
+  datasources: {
+    db: {
+      url: process.env.POSTGRES_PRISMA_URL,
+    },
+  },
+});
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
